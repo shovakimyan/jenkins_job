49ccccfad036d31e7fb9ec81ada9ef36fa944aa9
Sevak Hovakimyan <77205855+shovakimyan@users.noreply.github.com>
2022-10-22 02:53:42 +0400

    Update update_ami.groovy

--- update_ami.groovy
+++ update_ami.groovy
@@ -25,7 +25,7 @@ pipeline {
         stage('Update Launch Templates') {
         when { expression { return params.update_launch_template } }
             steps {
-                build job: ''aws/ec2/lunch_templates_sync'', parameters: [string(name: 'pipeline', value: ${params.pipeline_type}], propagate: false
+                build job: ''lunch_templates_sync'', parameters: [string(name: 'pipeline', value: ${params.pipeline_type}], propagate: false
                 }
             }
 
