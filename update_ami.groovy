3243c8745455e740a14584493c1f9260da357227
Sevak Hovakimyan <77205855+shovakimyan@users.noreply.github.com>
2022-10-22 22:52:49 +0400

    Update update_ami.groovy

--- update_ami.groovy
+++ update_ami.groovy
@@ -8,15 +8,9 @@ pipeline {
     }
 	
     parameters {
-        string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
 
-        text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
+        booleanParam(name: 'TOGGLE', defaultValue: false, description: 'Toggle this value')
 
-        booleanParam(name: 'TOGGLE', defaultValue: true, description: 'Toggle this value')
-
-        choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
-
-        password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
     }
 	
     stages {
@@ -33,7 +27,7 @@ pipeline {
 
 	    
 	stage('test cond') {
-     		when { expression { return env.AWS_DEFAULT_REGION } }
+     		when { expression { return params.TOGGLE } }
      		steps {
         	sh 'echo "running the stage"'
    	 	}
